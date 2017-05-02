122e533e66403fae4036af5884ce0cc3e044fb31
In Sang Song <isong2@cs.binghamton.edu>
2017-05-02 16:39:21 -0400

    Controller updated with constructor

--- controller.hpp
+++ controller.hpp
@@ -1,20 +1,56 @@
 #ifndef CONTROLLER_HPP
 #define CONTROLLER_HPP
 
-
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <sys/mman.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #include "superblock.h"
 
+#define handle_error(msg)			\
+  do{						\
+    perror(msg);				\
+    exit(EXIT_FAILURE);				\
+  } while(0)
+
+#define SIZE (sizeof(unsigned int) * buffer_size)
 
 class Controller{
+private:
+  //Current instruction pointer [][][][][][x][][][]
+  unsigned int current_instr;
   unsigned int buffer_size;
-  unsigned int current_inst;
-  int* shared_buffer_ptr;
+  int* ptr;
+  File* fh;
+  superblock sb;
+  
+public:
+  Controller(std::string filename; unsigned int buffer_size){
+    this->buffer_size = buffer_size;
 
-  
-  
-  
+    //initialize shared buffer
+    ptr = (int*)mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+    
+    //Memory not allocated
+    if(ptr < 0){
+      handle_error("shm open");
+    }
+
+    fh = fopen(filename, "wb+");
+    if(fh == NULL){
+      handle_error("file open");
+    }
+
+    //Rewind to locate 0, not necessary here, but may be necessary elsewhere
+    frewind();
+    fread(&sb, sizeof(superblock_t), 1, fh);
+
+  };
 };
 
 
