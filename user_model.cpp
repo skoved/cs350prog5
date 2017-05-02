58a4724b883d4493859970df1f24efd79d720732
skoved <sam.koved@gmail.com>
2017-05-02 19:09:59 -0400

    did stuff

--- user_model.cpp
+++ user_model.cpp
@@ -1,8 +1,16 @@
 #include "user_model.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
 
 void *user_model(void *arg){
-  
-
-  return NULL;
+    String fileName(arg[0]);
+    ifstream infile(fileName);
+    if(infile.is_open()){
+        string cmd;
+        while(!infile.eof()){
+            //read in line and store in vector
+        }
+    return NULL;
 }
 
