c0c12385cb8f5332f08679d53d01575beaf1d665
Curtis Ruan <cruan2@cs.binghamton.edu>
2017-05-04 14:50:44 -0400

    parsing

--- user_model.cpp
+++ user_model.cpp
@@ -2,15 +2,34 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <vector>
 
-void *user_model(void *arg){
-    String fileName(arg[0]);
-    ifstream infile(fileName);
-    if(infile.is_open()){
+using namespace std;
+/*
+	0. Create
+	1. Import
+	2. Cat
+	3. Delete
+	4. Write
+ */
+void *user_model(void * arg){
+	char* argument = (char*) arg[1];
+    string fileName(argument);
+    ifstream inFile(fileName);
+    if(inFile.is_open()){
         string cmd;
-        while(!infile.eof()){
+		vector<vector<string>> parsedCommands;
+		vector<string> command;  
+		char split_char = ' ';      
+		while(getline(inFile, cmd)){
             //read in line and store in vector
+			istringstream split(cmd);
+			command.clear();
+			for(string each; getline(split, each, split_char); command.push_back(each));
+			parsedCommands.push_back(command);
         }
+	}
     return NULL;
 }
 
