a72be8be17b28ce4c20591ff7137d045703b756e
goplemms <isong2@cs.binghamton.edu>
2017-05-06 20:55:25 -0400

    Nothing

--- ssfs.cpp
+++ ssfs.cpp
@@ -74,6 +74,28 @@ int main(int argc, char **argv){
       }
       disk.read(butt, 120, 10);
       break;
+    case 3:
+      butt = "amicable";
+      status = disk.create("cunts");
+      if(status == -1){
+	cerr << "Create is bad" << endl;
+      }
+      status = disk.create("fick");
+      if(status == -1){
+	cerr << "Create is bad" << endl;
+      }
+      status = disk.create("fickus");
+      if(status == -1){
+	cerr << "Create is bad" << endl;
+      }
+      status = disk.create("amicable");
+      if(status == -1){
+	cerr << "Create is bad" << endl;
+      }
+      disk.list();
+      disk.remove("fick");
+      disk.remove("fickus");
+      disk.remove("cunts");
     }
     disk.cat(butt);    
     disk.list();
@@ -89,28 +111,28 @@ int main(int argc, char **argv){
 }
 
 /*
-	0. Create
-	1. Import
-	2. Cat
-	3. Delete
-	4. Write
- */
+  0. Create
+  1. Import
+  2. Cat
+  3. Delete
+  4. Write
+*/
 void *user_model(void * arg){
-	char* argument = (char*) arg;
-    string fileName(argument);
-    ifstream inFile(fileName);
-    if(inFile.is_open()){
-        string cmd;
-		vector<vector<string>> parsedCommands;
-		vector<string> command;  
-		char split_char = ' ';      
-		while(getline(inFile, cmd)){
-            //read in line and store in vector
-			istringstream split(cmd);
-			command.clear();
-			for(string each; getline(split, each, split_char); command.push_back(each));
-			parsedCommands.push_back(command);
-        }
-	}
-    return NULL;
+  char* argument = (char*) arg;
+  string fileName(argument);
+  ifstream inFile(fileName);
+  if(inFile.is_open()){
+    string cmd;
+    vector<vector<string>> parsedCommands;
+    vector<string> command;  
+    char split_char = ' ';      
+    while(getline(inFile, cmd)){
+      //read in line and store in vector
+      istringstream split(cmd);
+      command.clear();
+      for(string each; getline(split, each, split_char); command.push_back(each));
+      parsedCommands.push_back(command);
+    }
+  }
+  return NULL;
 }
